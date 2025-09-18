6e4cb0f16284230fba88c8c57c1757351a3bed5f
re35t <2467065660@qq.com>
2025-09-18 11:28:22 +0800

    test

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,23 +1,37 @@
 // strings3.rs
 //
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
-// hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut start = 0;
+    let mut end = input.len() - 1;
+    let bytes = input.as_bytes();
+
+    while bytes[start] == b' ' {
+        start += 1;
+    }
+
+    while bytes[end] == b' ' {
+        end -= 1;
+    }
+
+    String::from(&input[start..=end])
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut result = input.to_string();
+
+    result.push_str(" world!");
+
+    result
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
